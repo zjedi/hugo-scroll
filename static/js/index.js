b664d26b0c2188baa1685a74bf526959a70597dc
Jan Raasch <jan@janraasch.com>
2020-09-21 13:35:37 +0200

    fix: quote icons
    icon-quote-left and icon-quote-right are only included on font-awesome-ie7.css

--- static/js/index.js
+++ static/js/index.js
@@ -106,8 +106,8 @@ var $sitehead = $("#site-head");
     $("ul li").before(
       '<span class="bult fa fa-asterisk icon-asterisk"></span>'
     );
-    $("blockquote p").prepend('<span class="quo icon-quote-left"></span>');
-    $("blockquote p").append('<span class="quo icon-quote-right"></span>');
+    $("blockquote p").prepend('<span class="quo fa fa-quote-left"></span>');
+    $("blockquote p").append('<span class="quo fa fa-quote-right"></span>');
   });
 
   $post.each(function () {
