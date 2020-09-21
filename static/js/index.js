90d0a1842357aaef7b06142eb73fc1e3eba9c998
Jan Raasch <jan@janraasch.com>
2020-09-21 13:37:16 +0200

    refactor: remove unneeded "icon-asterisk"-class

--- static/js/index.js
+++ static/js/index.js
@@ -104,7 +104,7 @@ var $sitehead = $("#site-head");
     }
 
     $("ul li").before(
-      '<span class="bult fa fa-asterisk icon-asterisk"></span>'
+      '<span class="bult fa fa-asterisk"></span>'
     );
     $("blockquote p").prepend('<span class="quo fa fa-quote-left"></span>');
     $("blockquote p").append('<span class="quo fa fa-quote-right"></span>');
